abe46d4c79adeafd3f9c3db5e069ec8c4eec5a34
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-05 18:05:24 -0500

    componente pay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.scss';
 import Header from './components/Header';
 import Slider from './components/Slider';
+import Pay from "./components/Pay";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <main>
         <Slider></Slider>
+        <Pay></Pay>
       </main>
     </div>
   );
---
+++ src/components/Pay.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import carroCompras from "../images/icon-cart.svg";
+import "../styles/_pay.scss";
+import agregar from "../images/icon-plus.svg"
+import eliminar from "../images/icon-minus.svg"
+
+function Pay() {
+  return (
+    <div className='contenedor-producto'>
+        <div className='descripcion-producto'>
+            <span className='subtitulo'>Sneaker Company</span>
+            <h1 className='titulo'>Fall Limited Edition Sneakers</h1>
+            <p className='descripcion'>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+                durable rubber outer sole, they’ll withstand everything the weather can offer.
+            </p>
+            <div className='contenedor-precio'>
+                <p className='precio'>$125.00 <span className='descuento' >50%</span></p>
+                <p className='precio-anterior'>$250.00</p>
+            </div>
+        </div>
+        <div className='contenedor-eleccion'>
+            <div className='resumen-compra'>
+                <img className='quitar-producto' src={eliminar} alt="icon eliminar" />
+                <p className='cantidad-producto'>0</p>
+                <img className='agregar-producto' src={agregar} alt="icon agregar" />
+            </div>
+            <button>
+                <img src={carroCompras} alt="logo carro de compras" />
+                Add to cart
+            </button>
+        </div>
+    </div>
+  );
+
+}
+export default Pay;
