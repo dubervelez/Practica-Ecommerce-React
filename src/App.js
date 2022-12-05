adb06ca6a5e27475335cbbf119a585e7e95f0616
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-05 16:44:02 -0500

    slider funcional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './styles/App.scss';
 import Header from './components/Header';
-
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <Slider></Slider>
+      </main>
     </div>
   );
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import btnImage1 from "../images/image-product-1-thumbnail.jpg"
+import btnImage2 from "../images/image-product-2-thumbnail.jpg"
+import btnImage3 from "../images/image-product-3-thumbnail.jpg"
+import btnImage4 from "../images/image-product-4-thumbnail.jpg"
+import producto1 from "../images/image-product-1.jpg"
+import producto2 from "../images/image-product-2.jpg"
+import producto3 from "../images/image-product-3.jpg"
+import producto4 from "../images/image-product-4.jpg"
+import next from "../images/icon-next.svg"
+import prev from "../images/icon-previous.svg"
+import "../styles/_slider.scss"
+import {useState} from "react";
+
+
+function Slider() {
+  
+    const [producto, setProducto] = useState(producto1)
+    const [btnactivo1, setbtnActivo1] = useState (true)
+    const [btnactivo2, setbtnActivo2] = useState (false)
+    const [btnactivo3, setbtnActivo3] = useState (false)
+    const [btnactivo4, setbtnActivo4] = useState (false)
+    function next1() {
+        setProducto(producto1)
+        setbtnActivo1(true)
+        setbtnActivo2(false)
+        setbtnActivo3(false)
+        setbtnActivo4(false)
+    }
+    function next2() {
+        setProducto(producto2)
+        setbtnActivo1(false)
+        setbtnActivo2(true)
+        setbtnActivo3(false)
+        setbtnActivo4(false)
+    }
+    function next3() {
+        setProducto(producto3)
+        setbtnActivo1(false)
+        setbtnActivo2(false)
+        setbtnActivo3(true)
+        setbtnActivo4(false)
+    }
+    function next4() {
+        setProducto(producto4)
+        setbtnActivo1(false)
+        setbtnActivo2(false)
+        setbtnActivo3(false)
+        setbtnActivo4(true)
+    }
+    
+    const lista = [producto1, producto2, producto3, producto4];
+    const nextmobile = ()=>{
+        for (let i = 0; i < lista.length; i++) {
+            let posicion = 0;
+            if (producto === lista[i]) {
+                posicion = Math.min(i, 2);
+                setProducto(lista[posicion+1]) 
+            }
+        }
+    }
+    const prevmobile = ()=>{
+        for (let i = 0; i < lista.length; i++) {
+            let posicion = 0;
+            if (producto === lista[i]) {
+                posicion = Math.max(i, 1);
+                setProducto(lista[posicion-1]) 
+            }
+        }
+    }
+
+
+
+
+    return (
+
+    <section className='slider'>
+        <div className="imagen-activa">
+            <img className='img-producto' src={producto} alt="imagen producto" />
+            <div className='btn-prev'>
+                <img  src={prev} alt="boton siguiente" onClick={prevmobile}/>
+            </div>
+            <div className='btn-next'>
+                <img src={next} alt="boton retroceder" onClick={nextmobile} />  
+            </div>
+        </div>
+        <div className="btn-slider">
+            <img className={`btn-img ${btnactivo1 ? "activo": "" }`} src={btnImage1} alt="imagen miniatura producto"  onClick={next1} />
+            <img className={`btn-img ${btnactivo2 ? "activo": "" }`} src={btnImage2} alt="imagen miniatura producto" onClick={next2}/>
+            <img className={`btn-img ${btnactivo3 ? "activo": "" }`} src={btnImage3} alt="imagen miniatura producto" onClick={next3}/>
+            <img className={`btn-img ${btnactivo4 ? "activo": "" }`} src={btnImage4} alt="imagen miniatura producto" onClick={next4}/>
+        </div>
+    </section>
+  )
+}
+
+export default Slider
