2e4c7ee7f01cfc5d73d1e9478f6906a9f112f464
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-04 23:28:48 -0500

    funcionalidad menu desplegable mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,28 @@ import menu from "../images/icon-menu.svg";
 import logo from "../images/logo.svg";
 import carroCompras from "../images/icon-cart.svg";
 import login from "../images/image-avatar.png";
-import cerrarMenu from "../images/icon-close.svg";
+import logoCerrarMenu from "../images/icon-close.svg";
 
 import "../styles/_header.scss";
+import { useState } from "react";
 
 function Header() {
+
+    const [menus, setMenu] = useState(false);
+    function desplegarMenu() {
+        setMenu(true);
+    };
+    function cerrarMenu() {
+        setMenu(false);
+    };
+
   return (
     <header>
         <nav>
-            <img className="logo-menu" src={menu} alt="logo menu" />
+            <img className="logo-menu" src={menu} alt="logo menu" onClick={desplegarMenu}/>
             <img className="logo-sneakers" src={logo} alt="logo Sneakers" />
-            <div className='nav-menu'>
-                <img className='logo-cerrar-menu' src={cerrarMenu} alt="logo cerrar menu" />
+            <div className={`nav-menu ${menus ? "activo": "" }`}>
+                <img className='logo-cerrar-menu' src={logoCerrarMenu} alt="logo cerrar menu" onClick={cerrarMenu}/>
                 <ul className="nav-ul">
                     <li className="nav-ul__li">Collections</li>
                     <li className="nav-ul__li">Men</li>
